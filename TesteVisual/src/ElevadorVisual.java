0b6293fb1cedcce95906f5f323baf21eb94109b0
Joao-August <joao.a.faria.nascimento@gmail.com>
2023-10-25 20:41:02 -0300

    Desenha mas não att a tela

--- TesteVisual/src/ElevadorVisual.java
+++ TesteVisual/src/ElevadorVisual.java
@@ -1,27 +1,20 @@
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-	static int numAndares = 10;
-	static int numPassageiros = 5;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch(args);
-		 //int numAndares = 10;
-	        int capacidadeElevador = 1;
-	        Predio predio = new Predio(numAndares, capacidadeElevador);
-	        Elevador elevador = predio.getElevador();
+public class ElevadorVisual extends Application {
+    private static final int NUM_ANDARES = 10; // Número de andares no prédio
+    private static final int NUM_PASSAGEIROS = 5; // Número de passageiros
 
-	        //int numPassageiros = 5;
-	        for (int i = 0; i < numPassageiros; i++) {
-	            Passageiro passageiro = new Passageiro(elevador, i, (i + 1) % numAndares);
-	            passageiro.run();
-	        }
-	}
-	public void start(Stage primaryStage) {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("Simulador de Elevador");
 
         Pane root = new Pane();
@@ -33,14 +26,14 @@ public class Main extends Application {
         root.getChildren().add(elevador);
 
         // Crie retângulos para representar os andares
-        for (int i = 0; i < numAndares; i++) {
+        for (int i = 0; i < NUM_ANDARES; i++) {
             Rectangle andar = new Rectangle(30, 400 - i * 40, 100, 5);
             andar.setStyle("-fx-fill: gray;");
             root.getChildren().add(andar);
         }
 
         // Crie retângulos para representar os passageiros
-        for (int i = 0; i < numPassageiros; i++) {
+        for (int i = 0; i < NUM_PASSAGEIROS; i++) {
             Rectangle passageiro = new Rectangle(10, 400, 20, 20);
             passageiro.setStyle("-fx-fill: red;");
             root.getChildren().add(passageiro);
